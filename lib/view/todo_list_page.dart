ad5476fad5d2bb9645ac831f7694cf06920938ec
YOSHITOMO7777 <zetuentai@gmail.com>
2024-02-04 15:54:11 +0900

    [add]チェックボックスに変更

--- lib/view/todo_list_page.dart
+++ lib/view/todo_list_page.dart
@@ -4,20 +4,22 @@ class ToDoList extends StatefulWidget {
   const ToDoList({super.key});
 
   @override
-  _ToDoListState createState() => _ToDoListState();
+  ToDoListState createState() => ToDoListState();
 }
 
-class _ToDoListState extends State<ToDoList> {
-  final List<String> _todoList = [];
+class ToDoListState extends State<ToDoList> {
+  final List<Map<String, dynamic>> _todoList = [];
   TextEditingController _textFieldController = TextEditingController();
 
+  // アイテム追加
   void _addToDoItem(String task) {
     setState(() {
-      _todoList.add(task);
+      _todoList.add({'task': task, 'isChecked': false});
     });
     _textFieldController.clear();
   }
 
+  // アイテム削除
   void _deleteToDoItem(int index) {
     setState(() {
       _todoList.removeAt(index);
@@ -25,13 +27,15 @@ class _ToDoListState extends State<ToDoList> {
     _textFieldController.clear();
   }
 
+  // アイテム編集
   void _editToDoItem(int index) {
     setState(() {
-      _todoList[index] = _textFieldController.text;
+      _todoList[index]['task'] = _textFieldController.text;
     });
     _textFieldController.clear();
   }
 
+  // ToDoリストの生成
   Widget _buildToDoList() {
     return ListView.builder(
       itemCount: _todoList.length,
@@ -41,31 +45,42 @@ class _ToDoListState extends State<ToDoList> {
     );
   }
 
-  Widget _buildToDoItem(String title, int index) {
-    return ListTile(
-      tileColor: Colors.blue[900],
-      title: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 20,
-          color: Colors.white,
-        ),
-      ),
-      onTap: () {
+  Widget _buildToDoItem(Map data, int index) {
+    return GestureDetector(
+      onLongPress: () {
         _displayDialog(context, index);
       },
-      trailing: IconButton(
-        icon: const Icon(
-          Icons.delete,
-          color: Colors.white,
+      child: CheckboxListTile(
+        tileColor: Colors.black12,
+        title: Text(
+          data['task'],
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: data['isChecked'] ? FontWeight.bold : FontWeight.normal,
+            color: data['isChecked'] ? Colors.blue : Colors.black,
+          ),
         ),
-        onPressed: () => _deleteToDoItem(index),
-      ),
+        secondary: IconButton(
+          icon: const Icon(
+            Icons.delete,
+            color: Colors.black45,
+          ),
+          onPressed: () => _deleteToDoItem(index),
+        ),
+        controlAffinity: ListTileControlAffinity.leading,
+        value: data['isChecked'],
+        onChanged: (bool? value) {
+          setState(() {
+            data['isChecked'] = value;
+          });
+        },
+        activeColor: Colors.blue[500],
+      )
     );
   }
 
   Future<void> _displayDialog(BuildContext context, int? index) async {
-    _textFieldController = TextEditingController(text: index == null ? '' : _todoList[index]);
+    _textFieldController = TextEditingController(text: index == null ? '' : _todoList[index]['task']);
     showDialog<AlertDialog>(
       context: context,
       builder: (BuildContext context) {
@@ -77,7 +92,7 @@ class _ToDoListState extends State<ToDoList> {
           ),
           actions: <Widget>[
             ElevatedButton(
-              child: Text(index == null ? 'Add' : 'Edit'),
+              child: Text(index == null ? 'Add' : 'Save'),
               onPressed: () {
                 if(index == null) {
                   _addToDoItem(_textFieldController.text);
